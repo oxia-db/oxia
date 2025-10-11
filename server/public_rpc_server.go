4f81f7294cabf5292849a9f77d00070b017c9861
Matteo Merli <mmerli@apache.org>
2025-10-10 17:43:28 -0700

    Fixed comment
    Signed-off-by: Matteo Merli <mmerli@apache.org>

--- server/public_rpc_server.go
+++ server/public_rpc_server.go
@@ -21,13 +21,14 @@ import (
 	"io"
 	"log/slog"
 
-	"github.com/oxia-db/oxia/proto/compat"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/encoding/protowire"
+
+	"github.com/oxia-db/oxia/proto/compat"
 
 	"github.com/oxia-db/oxia/common/concurrent"
 	"github.com/oxia-db/oxia/common/constant"
@@ -481,7 +482,7 @@ func (s *publicRpcServer) Close() error {
 	return s.grpcServer.Close()
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type compatPublicRpcServer struct {
 	compat.UnimplementedOxiaClientServer
