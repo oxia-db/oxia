9851837ad2d816a961f151118cb5b2688a68d1bb
Matteo Merli <mmerli@apache.org>
2025-10-10 17:03:07 -0700

    Switched Copyright to "The Oxia Authors" (#755)
    Signed-off-by: Matteo Merli <mmerli@apache.org>

--- server/public_rpc_server_test.go
+++ server/public_rpc_server_test.go
@@ -1,4 +1,4 @@
-// Copyright 2023 StreamNative, Inc.
+// Copyright 2023-2025 The Oxia Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,9 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// Additional attributions:
-// Copyright 2025 Maurice Barnum
 
 package server
 
