7ca94a190603df8f634980ea25a7add8472e0a67
Matteo Merli <mmerli@apache.org>
2025-10-15 16:35:38 -0700

    Removed no-lint comments
    Signed-off-by: Matteo Merli <mmerli@apache.org>

--- server/leader_controller.go
+++ server/leader_controller.go
@@ -820,13 +820,15 @@ func (lc *leaderController) write(ctx context.Context, requestSupplier func(offs
 
 	deferDbWrite := func(err error) {
 		if err != nil {
-			timer.Done() //nolint:contextcheck
+			//nolint:contextcheck
+			timer.Done()
 			cb.OnCompleteError(errors.Wrap(err, "oxia: failed to append to wal"))
 			return
 		}
 		tracker.AdvanceHeadOffset(newOffset)
 		tracker.WaitForCommitOffsetAsync(ctx, newOffset, concurrent.NewOnce[any](
-			func(_ any) { //nolint:contextcheck
+			//nolint:contextcheck
+			func(_ any) {
 				defer timer.Done()
 				var wr *proto.WriteResponse
 				if wr, err = lc.db.ProcessWrite(request, newOffset, timestamp, WrapperUpdateOperationCallback); err != nil {
@@ -834,7 +836,7 @@ func (lc *leaderController) write(ctx context.Context, requestSupplier func(offs
 					return
 				}
 				cb.OnComplete(wr)
-			}, func(err error) { //nolint:contextcheck
+			}, func(err error) {
 				timer.Done()
 				cb.OnCompleteError(errors.Wrap(err, "oxia: failed to append to wal"))
 			}))
