74f5aa8945ff70820a515459ca9e9d65c57a5b3c
Matteo Merli <mmerli@apache.org>
2025-10-24 14:55:01 -0700

    Minor clean up of using util methods in shard controller mocked tests (#787)
    Signed-off-by: Matteo Merli <mmerli@apache.org>

--- coordinator/controllers/shard_controller_test.go
+++ coordinator/controllers/shard_controller_test.go
@@ -183,6 +183,9 @@ func TestShardController_StartingWithLeaderAlreadyPresent(t *testing.T) {
 	s1 := model.Server{Public: "s1:9091", Internal: "s1:8191"}
 	s2 := model.Server{Public: "s2:9091", Internal: "s2:8191"}
 	s3 := model.Server{Public: "s3:9091", Internal: "s3:8191"}
+	n1 := rpc.GetNode(s1)
+	n2 := rpc.GetNode(s2)
+	n3 := rpc.GetNode(s3)
 
 	meta := metadata.NewMetadataProviderMemory()
 	defer meta.Close()
@@ -190,7 +193,6 @@ func TestShardController_StartingWithLeaderAlreadyPresent(t *testing.T) {
 	configResource := resources.NewClusterConfigResource(t.Context(), func() (model.ClusterConfig, error) {
 		return model.ClusterConfig{}, nil
 	}, nil, nil)
-	defer configResource.Close()
 
 	sc := NewShardController(constant.DefaultNamespace, shard, namespaceConfig, model.ShardMetadata{
 		Status:   model.ShardStatusSteadyState,
@@ -199,17 +201,12 @@ func TestShardController_StartingWithLeaderAlreadyPresent(t *testing.T) {
 		Ensemble: []model.Server{s1, s2, s3},
 	}, configResource, statusResource, nil, rpc)
 
-	select {
-	case <-rpc.GetNode(s1).newTermRequests:
-		assert.Fail(t, "shouldn't have received any newTerm requests")
-	case <-rpc.GetNode(s2).newTermRequests:
-		assert.Fail(t, "shouldn't have received any newTerm requests")
-	case <-rpc.GetNode(s3).newTermRequests:
-		assert.Fail(t, "shouldn't have received any newTerm requests")
+	n1.expectGetStatusRequest(t, shard)
+	n1.GetStatusResponse(1, proto.ServingStatus_LEADER, 0, 0)
 
-	case <-time.After(1 * time.Second):
-		// Ok
-	}
+	n1.expectNoMoreNewTermRequest(t)
+	n2.expectNoMoreNewTermRequest(t)
+	n3.expectNoMoreNewTermRequest(t)
 
 	assert.NoError(t, sc.Close())
 }
@@ -351,47 +348,21 @@ func TestShardController_VerifyFollowersWereAllFenced(t *testing.T) {
 		Ensemble: []model.Server{s1, s2, s3},
 	}, configResource, statusResource, nil, rpc)
 
-	r1 := <-n1.getStatusRequests
-	assert.EqualValues(t, 5, r1.Shard)
-	n1.getStatusResponses <- struct {
-		*proto.GetStatusResponse
-		error
-	}{&proto.GetStatusResponse{
-		Term:   4,
-		Status: proto.ServingStatus_LEADER,
-	}, nil}
+	n1.expectGetStatusRequest(t, 5)
+	n1.GetStatusResponse(4, proto.ServingStatus_LEADER, 0, 0)
 
-	r2 := <-n2.getStatusRequests
-	assert.EqualValues(t, 5, r2.Shard)
-	n2.getStatusResponses <- struct {
-		*proto.GetStatusResponse
-		error
-	}{&proto.GetStatusResponse{
-		Term:   4,
-		Status: proto.ServingStatus_FOLLOWER,
-	}, nil}
+	n2.expectGetStatusRequest(t, 5)
+	n2.GetStatusResponse(4, proto.ServingStatus_FOLLOWER, 0, 0)
 
 	// The `s3` server was not properly fenced and it's stuck term 3
 	// It needs to be fenced again
-	r3 := <-n3.getStatusRequests
-	assert.EqualValues(t, 5, r3.Shard)
-	n3.getStatusResponses <- struct {
-		*proto.GetStatusResponse
-		error
-	}{&proto.GetStatusResponse{
-		Term:   3,
-		Status: proto.ServingStatus_FOLLOWER,
-	}, nil}
+	n3.expectGetStatusRequest(t, 5)
+	n3.GetStatusResponse(3, proto.ServingStatus_FOLLOWER, 0, 0)
 
 	// This should have triggered a new election, since s3 was in the wrong term
-	nt1 := <-n1.newTermRequests
-	assert.EqualValues(t, 5, nt1.Term)
-
-	nt2 := <-n2.newTermRequests
-	assert.EqualValues(t, 5, nt2.Term)
-
-	nt3 := <-n3.newTermRequests
-	assert.EqualValues(t, 5, nt3.Term)
+	n1.expectNewTermRequest(t, shard, 5, true)
+	n2.expectNewTermRequest(t, shard, 5, true)
+	n3.expectNewTermRequest(t, shard, 5, true)
 
 	assert.NoError(t, sc.Close())
 }
