f8edcd91dc1da150f7ea0f29f95d4a3a9df0443f
mattisonchao <mattisonchao@gmail.com>
2025-12-03 23:42:54 +0800

    fixes: wrong package import

--- oxiad/coordinator/metadata/k8s_client.go
+++ oxiad/coordinator/metadata/k8s_client.go
@@ -20,9 +20,9 @@ import (
 	"log/slog"
 	"os"
 
-	"github.com/pingcap/errors"
 	pb "google.golang.org/protobuf/proto"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
