9aab061f7575caf6a2ca487766f6f24e20fff62c
mattisonchao <mattisonchao@gmail.com>
2026-01-17 23:07:14 +0800

    add watch test

---
+++ oxiad/common/option/watch_test.go
@@ -0,0 +1,305 @@
+// Copyright 2023-2025 The Oxia Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package option
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewWatch(t *testing.T) {
+	w := NewWatch("initial")
+
+	value, version := w.Load()
+	assert.Equal(t, "initial", value)
+	assert.Equal(t, uint64(0), version)
+}
+
+func TestWatchLoad(t *testing.T) {
+	w := NewWatch[int](42)
+
+	value, version := w.Load()
+	assert.Equal(t, 42, value)
+	assert.Equal(t, uint64(0), version)
+}
+
+func TestWatchNotify(t *testing.T) {
+	w := NewWatch("initial")
+
+	w.Notify("updated")
+
+	value, version := w.Load()
+	assert.Equal(t, "updated", value)
+	assert.Equal(t, uint64(1), version)
+}
+
+func TestWatchNotifyMultipleTimes(t *testing.T) {
+	w := NewWatch[int](0)
+
+	for i := 1; i <= 5; i++ {
+		w.Notify(i * 10)
+		value, version := w.Load()
+		assert.Equal(t, i*10, value)
+		assert.Equal(t, uint64(i), version)
+	}
+}
+
+func TestWaitWithNewerVersion(t *testing.T) {
+	w := NewWatch("initial")
+
+	w.Notify("updated")
+
+	value, version := w.Wait(0)
+	assert.Equal(t, "updated", value)
+	assert.Equal(t, uint64(1), version)
+}
+
+func TestWaitWithSameVersion(t *testing.T) {
+	w := NewWatch("initial")
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		value, version := w.Wait(0)
+		assert.Equal(t, "updated", value)
+		assert.Equal(t, uint64(1), version)
+	}()
+
+	time.Sleep(10 * time.Millisecond)
+	w.Notify("updated")
+
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		t.Fatal("Wait didn't complete after notification")
+	}
+}
+
+func TestWaitConcurrent(t *testing.T) {
+	w := NewWatch[int](0)
+
+	var wg sync.WaitGroup
+	results := make([]struct {
+		value   int
+		version uint64
+	}, 5)
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			value, version := w.Wait(0)
+			results[idx] = struct {
+				value   int
+				version uint64
+			}{value: value, version: version}
+		}(i)
+	}
+
+	time.Sleep(10 * time.Millisecond)
+	w.Notify(100)
+
+	wg.Wait()
+
+	for i := 0; i < 5; i++ {
+		assert.Equal(t, 100, results[i].value)
+		assert.Equal(t, uint64(1), results[i].version)
+	}
+}
+
+func TestWaitMultipleNotification(t *testing.T) {
+	w := NewWatch[int](0)
+
+	done1 := make(chan struct{})
+	go func() {
+		defer close(done1)
+		value, version := w.Wait(0)
+		assert.Equal(t, 100, value)
+		assert.Equal(t, uint64(1), version)
+	}()
+
+	time.Sleep(10 * time.Millisecond)
+	w.Notify(100)
+
+	select {
+	case <-done1:
+	case <-time.After(time.Second):
+		t.Fatal("First wait didn't complete")
+	}
+
+	done2 := make(chan struct{})
+	go func() {
+		defer close(done2)
+		value, version := w.Wait(1)
+		assert.Equal(t, 200, value)
+		assert.Equal(t, uint64(2), version)
+	}()
+
+	time.Sleep(10 * time.Millisecond)
+	w.Notify(200)
+
+	select {
+	case <-done2:
+	case <-time.After(time.Second):
+		t.Fatal("Second wait didn't complete")
+	}
+}
+
+func TestWatchWithStruct(t *testing.T) {
+	type TestStruct struct {
+		Name string
+		Age  int
+	}
+
+	initial := TestStruct{Name: "Alice", Age: 25}
+	w := NewWatch(initial)
+
+	value, version := w.Load()
+	assert.Equal(t, initial, value)
+	assert.Equal(t, uint64(0), version)
+
+	updated := TestStruct{Name: "Bob", Age: 30}
+	w.Notify(updated)
+
+	value, version = w.Load()
+	assert.Equal(t, updated, value)
+	assert.Equal(t, uint64(1), version)
+}
+
+func TestWatchConcurrentLoadAndNotify(t *testing.T) {
+	w := NewWatch[int](0)
+	var wg sync.WaitGroup
+
+	// Concurrent loaders
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100; j++ {
+				w.Load()
+			}
+		}()
+	}
+
+	// Concurrent notifiers
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			for j := 0; j < 10; j++ {
+				w.Notify(idx*100 + j)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
+	_, finalVersion := w.Load()
+	assert.GreaterOrEqual(t, finalVersion, uint64(50))
+}
+
+func TestWaitRaceCondition(t *testing.T) {
+	w := NewWatch[int](0)
+
+	var wg sync.WaitGroup
+	done := make(chan bool, 100)
+
+	// Start many waiters
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			value, version := w.Wait(0)
+			if value == 999 && version == 1 {
+				done <- true
+			}
+		}(i)
+	}
+
+	time.Sleep(10 * time.Millisecond)
+	w.Notify(999)
+
+	wg.Wait()
+
+	// All waiters should have been notified
+	assert.Len(t, done, 100)
+}
+
+func TestWatchVersionIncrement(t *testing.T) {
+	w := NewWatch[string]("")
+
+	assert.Equal(t, uint64(0), getVersion(w))
+
+	w.Notify("v1")
+	assert.Equal(t, uint64(1), getVersion(w))
+
+	w.Notify("v2")
+	assert.Equal(t, uint64(2), getVersion(w))
+
+	w.Notify("v3")
+	assert.Equal(t, uint64(3), getVersion(w))
+}
+
+func TestWatchWithNilInterface(t *testing.T) {
+	var w *Watch[string]
+
+	assert.Panics(t, func() {
+		w.Load()
+	})
+
+	assert.Panics(t, func() {
+		w.Wait(0)
+	})
+
+	assert.Panics(t, func() {
+		w.Notify("")
+	})
+}
+
+func getVersion[T any](w *Watch[T]) uint64 {
+	_, version := w.Load()
+	return version
+}
+
+func BenchmarkWatchLoad(b *testing.B) {
+	w := NewWatch[int](42)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		w.Load()
+	}
+}
+
+func BenchmarkWatchNotify(b *testing.B) {
+	w := NewWatch[int](0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		w.Notify(i)
+	}
+}
+
+func BenchmarkWatchWaitImmediate(b *testing.B) {
+	w := NewWatch[int](0)
+	w.Notify(1)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		w.Wait(0)
+	}
+}
