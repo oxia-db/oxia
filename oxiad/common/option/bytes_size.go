316fe98cf462cacff2d818babedd448b80aff19c
mattisonchao <mattisonchao@gmail.com>
2026-01-27 16:09:15 +0800

    feat: support configurable write cache

--- oxiad/common/option/bytes_size.go
+++ oxiad/common/option/bytes_size.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/docker/go-units"
+	"github.com/invopop/jsonschema"
 	"gopkg.in/yaml.v3"
 )
 
@@ -24,4 +25,20 @@ func (b *BytesSize) UnmarshalYAML(value *yaml.Node) error {
 
 func (b BytesSize) MarshalYAML() (interface{}, error) {
 	return units.BytesSize(float64(b)), nil
+}
+
+// JSONSchema implements the jsonschema.ReflectType interface.
+// Notice: We have no choice but to use the value receiver here.
+func (BytesSize) JSONSchema() *jsonschema.Schema {
+	return &jsonschema.Schema{
+		Type:        "string",
+		Title:       "Bytes Size",
+		Description: "Bytes size string in human-readable format (e.g., \"1KiB\", \"100MiB\", \"2GiB\")",
+		Pattern:     "^([0-9]+(\\.[0-9]+)?(B|KB|K|MB|M|GB|G|TB|T|PB|P|KiB|MiB|GiB|TiB|PiB))+$",
+		Examples: []any{
+			"1KiB",
+			"100MiB",
+			"2GiB",
+		},
+	}
 }
