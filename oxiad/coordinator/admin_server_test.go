a9a1d7d70d1bb9b9bebcb513e3d4b9b02a540b4b
mattisonchao <mattisonchao@gmail.com>
2026-04-13 21:25:05 +0800

    Tighten ListDataServers test

--- oxiad/coordinator/admin_server_test.go
+++ oxiad/coordinator/admin_server_test.go
@@ -22,36 +22,16 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/oxia-db/oxia/common/proto"
-	"github.com/oxia-db/oxia/oxiad/coordinator/metadata"
 	"github.com/oxia-db/oxia/oxiad/coordinator/model"
-	"github.com/oxia-db/oxia/oxiad/coordinator/resource"
 )
 
 func TestAdminServerListDataServers(t *testing.T) {
 	serverName1 := "server-1"
 	serverName2 := "server-2"
 	serverName3 := "server-3"
-	meta := metadata.NewMetadataProviderMemory()
-	_, err := meta.Store(&model.ClusterStatus{
-		Namespaces: map[string]model.NamespaceStatus{
-			"default": {
-				Shards: map[int64]model.ShardMetadata{
-					1: {
-						Ensemble: []model.Server{
-							{Name: &serverName1, Public: "public-1", Internal: "internal-1"},
-							{Name: &serverName2, Public: "public-2", Internal: "internal-2"},
-							{Name: &serverName3, Public: "public-3", Internal: "internal-3"},
-						},
-					},
-				},
-			},
-		},
-	}, metadata.NotExists)
-	require.NoError(t, err)
-	statusResource := resource.NewStatusResource(meta)
 
 	admin := newAdminServer(
-		statusResource,
+		nil,
 		func() (model.ClusterConfig, error) {
 			return model.ClusterConfig{
 				Servers: []model.Server{
@@ -68,15 +48,18 @@ func TestAdminServerListDataServers(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, res.DataServers, 3)
 
-	assert.Equal(t, &serverName1, res.DataServers[0].Name)
+	require.NotNil(t, res.DataServers[0].Name)
+	assert.Equal(t, serverName1, *res.DataServers[0].Name)
 	assert.Equal(t, "public-1", res.DataServers[0].PublicAddress)
 	assert.Equal(t, "internal-1", res.DataServers[0].InternalAddress)
 
-	assert.Equal(t, &serverName2, res.DataServers[1].Name)
+	require.NotNil(t, res.DataServers[1].Name)
+	assert.Equal(t, serverName2, *res.DataServers[1].Name)
 	assert.Equal(t, "public-2", res.DataServers[1].PublicAddress)
 	assert.Equal(t, "internal-2", res.DataServers[1].InternalAddress)
 
-	assert.Equal(t, &serverName3, res.DataServers[2].Name)
+	require.NotNil(t, res.DataServers[2].Name)
+	assert.Equal(t, serverName3, *res.DataServers[2].Name)
 	assert.Equal(t, "public-3", res.DataServers[2].PublicAddress)
 	assert.Equal(t, "internal-3", res.DataServers[2].InternalAddress)
 }
