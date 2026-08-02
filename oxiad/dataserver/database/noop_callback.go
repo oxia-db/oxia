13900efeccc94422acae92150dc378c835d1220c
Qiang Zhao <mattisonchao@apache.org>
2026-08-02 13:58:29 +0800

    refactor: extract no-op database callback (#1270)
    ## Motivation
    
    `db.go` currently contains both the database implementation and the
    no-op update callback. Keeping the callback in its own file makes the
    callback implementation easier to find and keeps this mechanical cleanup
    independent from the secondary-index validation work.
    
    ## Modifications
    
    - move `noopCallback` and `NoOpCallback` from `db.go` to
    `noop_callback.go`
    - preserve the existing callback behavior and public API
    
    ## Testing
    
    - `go test ./oxiad/dataserver/...`
    - `make lint`
    - `git diff --check`
    
    Signed-off-by: mattisonchao <mattisonchao@gmail.com>

---
+++ oxiad/dataserver/database/noop_callback.go
@@ -0,0 +1,40 @@
+// Copyright 2023-2026 The Oxia Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package database
+
+import (
+	"github.com/oxia-db/oxia/common/proto"
+	"github.com/oxia-db/oxia/oxiad/dataserver/database/kvstore"
+)
+
+type noopCallback struct{}
+
+func (*noopCallback) OnDeleteWithEntry(kvstore.WriteBatch, *Notifications, string, *proto.StorageEntry) error {
+	return nil
+}
+
+func (*noopCallback) OnPut(_ kvstore.WriteBatch, _ *Notifications, _ *proto.PutRequest, _ *proto.StorageEntry) (proto.Status, error) {
+	return proto.Status_OK, nil
+}
+
+func (*noopCallback) OnDelete(_ kvstore.WriteBatch, _ *Notifications, _ string) error {
+	return nil
+}
+
+func (*noopCallback) OnDeleteRange(_ kvstore.WriteBatch, _ *Notifications, _ string, _ string) error {
+	return nil
+}
+
+var NoOpCallback UpdateOperationCallback = &noopCallback{}
